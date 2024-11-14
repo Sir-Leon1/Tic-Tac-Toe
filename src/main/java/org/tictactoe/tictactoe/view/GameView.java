01b14a1aa667a197552b49d4e0d68a75fcd143e8
cyrus <eb3.61528.22@student.chuka.ac.ke>
2024-11-14 23:25:37 +0300

    Added GameView and Styled using CSS

--- src/main/java/org/tictactoe/tictactoe/view/GameView.java
+++ src/main/java/org/tictactoe/tictactoe/view/GameView.java
@@ -17,23 +17,13 @@ public class GameView {
         root.setHgap(10);
         root.setPadding(new Insets(10, 10, 10, 10));
         root.getStyleClass().add("grid-pane");
-        //root.setStyle("-fx-background-color: dark #d4dc05;");
-        // Ensure the image file is located at src/main/resources/images/example.png
+
+        // Adding profile image
         Image image = new Image(getClass().getResource("/profile.png").toExternalForm());
         ImageView profileView = new ImageView(image);
-        //root.getChildren().add(imageView);
         profileView.getStyleClass().add("profile-image");
         profileView.setFitHeight(100);
         profileView.setFitWidth(100);
-
-        Separator separator = new Separator();
-        separator.setPrefWidth(800);
-
-        Button button = new Button("New Game");
-        button.getStyleClass().add("custom-button");
-        button.setOnAction(e -> {
-            System.out.println("Hello, World!");
-        });
 
         //Username Label
         Label username = new Label("Username: ");
@@ -41,11 +31,40 @@ public class GameView {
         //friends Label
         Label friends = new Label("Friends: ");
         friends.getStyleClass().add("username");
+
+        Separator separator = new Separator();
+        separator.getStyleClass().add("custom-separator");
+        separator.setPrefWidth(Double.MAX_VALUE);
+
+        //NewGame Button
+        Button button = new Button("New Game");
+        button.getStyleClass().add("custom-button");
+        button.setOnAction(e -> {
+            System.out.println("Hello, World!");
+        });
+
+        //High score Label
+        Label highScore = new Label("HighScore: ");
+        highScore.getStyleClass().add("high-score");
+
+
+        //Reset Button
+        Button resetButton = new Button("Reset");
+        resetButton.getStyleClass().add("custom-button");
+
+        //Log Out Button
+        Button logOutButton = new Button("LogOut");
+        logOutButton.getStyleClass().add("custom-button");
+
+
         root.add(profileView, 0, 0, 2, 2);
         root.add(username, 2, 0);
         root.add(friends, 2, 1);
-        root.add(separator,3,1, 3,3);
-        root.add(button, 3,1);
+        root.add(separator,0,2, 4,1);
+        root.add(button, 0,3);
+        root.add(highScore, 0,4);
+        root.add(resetButton, 0,5);
+        root.add(logOutButton, 0, 6);
 
         return root;
     }
